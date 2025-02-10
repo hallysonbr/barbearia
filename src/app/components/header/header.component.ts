8f167ab04afceb4e493700133666e81f7c52ba01
Hallyson Bruno <hallyson.bruno@pottencial.com.br>
2025-02-10 17:02:30 -0300

    Add a submenu for responsive screens

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -7,10 +8,16 @@ import { Router } from '@angular/router';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  menuOpen = false;
 
-  constructor(private router: Router) {}
+  constructor(private authService: AuthService, private router: Router) {}
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
 
   logout() {
+    this.authService.logout();
     this.router.navigate(['/login']);
   }
 }
